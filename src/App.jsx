921f87d2f4b51e2e53b3606a471986cfeee07851
anhMinhDepTraiQua <ducanhbanche@gmail.com>
2026-01-14 05:27:13 +0700

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./Home.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter basename="Nodejs-day-1">
+    <BrowserRouter basename="/nodejs-day-1/">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
